7b5dd265833555efbc7e963366893824826a8caa
Rijul Marhatta <102784647+RijulMarhatta@users.noreply.github.com>
2022-04-01 16:33:40 +0530

    Add files via upload

---
+++ PYTHON PROGRAM FOR SIMPLE INTERST.py
@@ -0,0 +1,6 @@
+# PYTHON PROGRAM FOR SIMPLE INTERST
+p = int(input("enter the principle amount:"))
+r = float(input("enter the rate of interest in decimal form:"))
+t = int(input("enter the number of years"))
+x = p*r*t
+print(f"the simple interest is {x}")
