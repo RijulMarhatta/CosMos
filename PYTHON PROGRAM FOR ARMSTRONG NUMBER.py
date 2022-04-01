de984dd7fc3a8522d9bc4d509170c91ce554d400
Rijul Marhatta <102784647+RijulMarhatta@users.noreply.github.com>
2022-04-01 16:36:05 +0530

    Create PYTHON PROGRAM FOR ARMSTRONG NUMBER.py

---
+++ PYTHON PROGRAM FOR ARMSTRONG NUMBER.py
@@ -0,0 +1,6 @@
+#PYTHON PROGRAM FOR ARMSTRONG NUMBER
+a=int(input("enter the first digit:"))
+b=int(input("enter the second digit:"))
+c=int(input("enter the third digit:"))
+x=pow((a),3)+pow((b),3)+pow((c),3)
+print(f"the value typed is {a}{b}{c},and the armstrong vale is {x}:")
