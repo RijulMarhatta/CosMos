6cc93f38a68a733014ec25885351276685206e82
RijulMarhatta <102784647+RijulMarhatta@users.noreply.github.com>
2022-04-01 12:08:06 +0530

    Add files via upload

---
+++ max numbers.py
@@ -0,0 +1,5 @@
+#PYTHON PROGRAM TO FIND MAXIMUM OF TWO NUMBERS
+num1=int(input('enter a number:'))
+num2=int(input('enter another number:'))
+maximum=max(num1,num2)
+print(f"the maximum value between the numbers is {maximum}")
